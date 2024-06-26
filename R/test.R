eb19f08fcbc924b063ba7419921b120fde52e55a
mnrzrad <mina.norouzirad@gmail.com>
2024-06-26 19:23:50 +0100

    update

--- R/test.R
+++ R/test.R
@@ -33,39 +33,41 @@
 
 # https://journal.r-project.org/articles/RJ-2023-067/RJ-2023-067.pdf
 
-# params <- c(1,5,1,5,0)
-# sim_data <- rBPGC(params, points = 1e4, seed = 42)
-# X <- sim_data$x
-# Y <- sim_data$y
+params <- c(1,5,1,5,1)
+sim_data <- rBPGC(params, points = 1e4, seed = 42)
+X <- sim_data$x
+Y <- sim_data$y
+
+estimated_params <-mleEst(X,Y)$params
+
+set.seed(42)
+theoretical_data <- rBPGC(estimated_params, points = 1e4)
+X_theoretical <- theoretical_data$x
+Y_theoretical <- theoretical_data$y
+
+S3 <- cbind(X, Y)
+S4 <- cbind(X_theoretical, Y_theoretical)
+
+fasano.franceschini.test(S3, S4, seed = 2, verbose = FALSE)
 #
-# estimated_params <-mleEst(X,Y)$params
-#
-# set.seed(42)
-# theoretical_data <- rBPGC(estimated_params, points = 1e4)
-# X_theoretical <- theoretical_data$x
-# Y_theoretical <- theoretical_data$y
-#
-# S3 <- cbind(X, Y)
-# S4 <- cbind(X_theoretical, Y_theoretical)
-#
-# fasano.franceschini.test(S3, S4, seed = 2, verbose = FALSE)
-#
-# library(fasano.franceschini.test)
-# library(readr)
-# data <- read_csv("data.csv")
-# index <- sample(1:nrow(data), 10000)
-# data <- data[index,]
-# X <- data$X
-# Y <- data$Y
-#
-# estimated_params <-mlEst(X,Y)$params
-#
-# set.seed(42)
-# theoretical_data <- rBPGC(estimated_params, points = nrow(data))
-# X_theoretical <- theoretical_data$x
-# Y_theoretical <- theoretical_data$y
-#
-# S3 <- cbind(X, Y)
-# S4 <- cbind(X_theoretical, Y_theoretical)
-#
-# fasano.franceschini.test(S3, S4, seed = 2, verbose = FALSE)
+library(fasano.franceschini.test)
+library(readr)
+data <- read_csv("data.csv")
+index <- sample(1:nrow(data), 10000)
+data <- data[index,]
+X <- data$X
+Y <- data$Y
+
+estimated_params <-mleEst(X,Y)$params
+
+set.seed(42)
+theoretical_data <- rBPGC(estimated_params, points = nrow(data))
+X_theoretical <- theoretical_data$x
+Y_theoretical <- theoretical_data$y
+
+S3 <- cbind(X, Y)
+S4 <- cbind(X_theoretical, Y_theoretical)
+
+fasano.franceschini.test(S3, S4, seed = 2, verbose = FALSE)
+
+ePLOT(X, Y, estimated_params)
